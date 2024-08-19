9e5b07dad06e2fd049e9e0161b5d243a87871221
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-08-19 13:51:30 +0530

    chore: Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { twMerge } from "tailwind-merge";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Wave Disonance",
+  description: "SAAS Landing Page",
 };
 
 export default function RootLayout({
