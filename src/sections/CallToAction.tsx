d1e54e19b1d54df140507d5cb7ea2b09f29eb56c
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-08-20 11:30:29 +0530

    feat: Call to action section parallax

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import ArrowRight from "@/assets/arrow-right.svg";
 import starImage from "@/assets/star.png";
 import springImage from "@/assets/spring.png";
 import Image from "next/image";
+import { motion as m, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="overflow-x-clip bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+    <section
+      ref={sectionRef}
+      className="overflow-x-clip bg-gradient-to-b from-white to-[#D2DCFF] py-24"
+    >
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title">Sign up for free today</h2>
@@ -13,17 +29,19 @@ export const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
-          <Image
-            src={starImage}
+          <m.img
+            src={starImage.src}
             alt="Star Image"
             width={360}
             className="absolute -left-[350px] -top-[137px]"
+            style={{ translateY }}
           />
-          <Image
-            src={springImage}
+          <m.img
+            src={springImage.src}
             alt="Spring Image"
             width={360}
             className="absolute -right-[331px] -top-[19px]"
+            style={{ translateY }}
           />
         </div>
         <div className="mt-10 flex justify-center gap-2">
